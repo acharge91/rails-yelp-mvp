3221dfc1c1a243af6a7fdc784019aca4591616bc
Ashton Charge <ashtoncharge@gmail.com>
2022-02-16 14:55:54 +0000

    rails yelp mvp

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  RATING = (0..5)
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: RATING }
+end
